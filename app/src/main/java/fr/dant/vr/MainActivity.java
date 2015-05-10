b3c13365e8a22e3d0c48a1cdc607760dc6d26288
nasser ashraf <serranashraf@gmail.com>
2015-05-11 01:11:38 +0200

    interface sign up

--- app/src/main/java/fr/dant/vr/MainActivity.java
+++ app/src/main/java/fr/dant/vr/MainActivity.java
@@ -1,10 +1,16 @@
 package fr.dant.vr;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -13,5 +19,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn_main_signin);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("MainActivity", "Result_KO");
+                Toast.makeText(MainActivity.this, "RESULT_KO", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
